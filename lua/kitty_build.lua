265ac41bd24320656723df55ee4c0ec71de91fda
Ryan Schlesinger <ryan@ryanschlesinger.com>
2022-02-07 10:18:29 -0400

    Fix "bright" colors

--- lua/kitty_build.lua
+++ lua/kitty_build.lua
@@ -1,4 +1,7 @@
 local colorscheme = require("lunarized").colors
+local run = require("shipwright").run
+local kitty = require("shipwright.transform.contrib.kitty")
+local overwrite = require("shipwright.transform.overwrite")
 
 run(colorscheme,
   -- Generage kitty input format
@@ -10,7 +13,9 @@ run(colorscheme,
       cursor_bg = colors.base0,
       selection_fg = colors.base01,
       selection_bg = colors.base03,
+
       url = colors.violet,
+
       black = colors.base03,
       red = colors.red,
       green = colors.green,
@@ -19,14 +24,21 @@ run(colorscheme,
       magenta = colors.magenta,
       cyan = colors.cyan,
       white = colors.base3,
-      bright_black = colors.base03,
-      bright_red = colors.red,
-      bright_green = colors.green,
-      bright_yellow = colors.yellow,
-      bright_blue = colors.blue,
-      bright_magenta = colors.magenta,
-      bright_cyan = colors.cyan,
+      bright_black = colors.base01,
+      bright_red = colors.orange,
+      bright_green = colors.base01,
+      bright_yellow = colors.base00,
+      bright_blue = colors.base0,
+      bright_magenta = colors.violet,
+      bright_cyan = colors.base0,
       bright_white = colors.base3,
+
+      titlebar = colors.base03.darken(15),
+      tab_active_fg = colors.base1,
+      tab_active_bg = colors.base02,
+      tab_inactive_fg = colors.base0,
+      tab_inactive_bg = colors.base03.darken(30),
+
       name = "Lunarized",
       author = "Ryan Schlesinger",
       upstream = "https://github.com/ryansch/lunarized",
@@ -35,7 +47,7 @@ run(colorscheme,
   end,
 
   -- now pass to kitty
-  {contrib.kitty},
+  {kitty},
 
   -- and now we can write, either to share or to our local config
   {overwrite, vim.fn.expand("~/.dotfiles/kitty/lunarized.conf")})
